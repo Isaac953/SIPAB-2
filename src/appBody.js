76d118641c26615fa80193108139566f2a639c5b
Isaac <isaac.arias07@gmail.com>
2018-08-08 23:36:45 -0600

    Agregando archivos de la APP

---
+++ src/appBody.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+export default class AppBody extends Component {
+render() {
+return (
+   <Text>
+      Body Content
+   </Text>
+);
+}
+}
+module.export = AppBody;
